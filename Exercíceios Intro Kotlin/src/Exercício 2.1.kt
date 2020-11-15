822de368f61d5bc57c2b4b7f79aafc33299da163
ironman <daivid.v.leal@gmail.com>
2020-11-15 13:42:45 -0300

    change function Exercício 2.1.kt to not modify other exercises that use getInt

--- Exercíceios Intro Kotlin/src/Exercício 2.1.kt
+++ Exercíceios Intro Kotlin/src/Exercício 2.1.kt
@@ -9,21 +9,29 @@
 val arrayAges = arrayListOf<Int>()
 
 fun main() {
-    val manOlder = getIntValue("Digite a idade do homem mais velho: ", "Idade Inválida.")
-    val manYounger = getIntValue("Digite a idade do homem mais novo: ", "Idade Inválida.")
-    val womenOlder = getIntValue("Digite a idade da mulher mais velha: ", "Idade Inválida.")
-    val womenYounger = getIntValue("Digite a idade da mulher mais nova: ", "Idade Inválida.")
+    val manOlder = getNonRepeatedValue("Digite a idade do homem mais velho: ", "Idade Inválida.")
+    val manYounger = getNonRepeatedValue("Digite a idade do homem mais novo: ", "Idade Inválida.")
+    val womenOlder = getNonRepeatedValue("Digite a idade da mulher mais velha: ", "Idade Inválida.")
+    val womenYounger = getNonRepeatedValue("Digite a idade da mulher mais nova: ", "Idade Inválida.")
 
     println("A soma das idades do homem mais velho com a mulher mais nova é: ${manOlder + womenYounger}.")
     println("O produto das idades do homem mais novo com a mulher mais velha é: ${manYounger * womenOlder}.")
 }
 
+fun getNonRepeatedValue(message: String, errorMessage: String): Int{
+    var value = getIntValue(message, errorMessage)
+    while (value in arrayAges){
+        value = getIntValue(message, errorMessage)
+    }
+    arrayAges.add(value)
+    return value
+}
+
 fun getIntValue(message: String, errorMessage: String): Int{
     var value = message.read().toIntOrNull() ?: 0
-    while (value <= 0 || value in arrayAges) {
+    while (value <= 0) {
         println(errorMessage)
         value = message.read().toIntOrNull() ?: 0
     }
-    arrayAges.add(value)
     return value
 }
